792cbb8c73f322aec937a249ccc9db9d63135669
agent-52 <abhaybhadauriya52@gmail.com>
2026-08-18 18:53:11 +0530

    redeploy with production api url

--- Frontend/src/api/axiosApi.ts
+++ Frontend/src/api/axiosApi.ts
@@ -5,7 +5,7 @@ import { logout, refreshToken } from "../features/auth/api";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
 });
 
